cb2cefae98cec600020a8b654a588500c4c3ddee
Vitor Mattos <vitor.mattos@phprio.org>
2016-08-22 22:34:03 -0300

    teste

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ require_once 'config.php';
 if(getenv('MODE_ENV') == 'develop') {
     class mockApi extends Api{
         public function getWebhookUpdates($emitUpdateWasReceivedEvent = true) {
-            $json = '{"update_id":2058587,"message":{"message_id":3785,"from":{"id":147949488,"first_name":"Jos\u00e9","last_name":"das Couves","username":"vitorphp"},"chat":{"id":147949488,"first_name":"Jos\u00e9","last_name":"das Couves","username":"vitorphp","type":"private"},"date":1463402259,"reply_to_message":{"message_id":3784,"from":{"id":180956067,"first_name":"feedbackbot","username":"Feedbackcoolbot"},"chat":{"id":147949488,"first_name":"Jos\u00e9","last_name":"das Couves","username":"vitorphp","type":"private"},"date":1463402235,"text":"\u2764\ufe0f Fa\u00e7a um coment\u00e1rio e reconhe\u00e7a o esfor\u00e7o do seu colega: \/profile_37900977","entities":[{"type":"bot_command","offset":59,"length":17}]},"text":"Identificado"}}';
+            $json = '{}';
             return new Update(json_decode($json, true));
         }
     }
